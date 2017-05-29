fd6b2dbf8e32d2d1c5c054f7492e953f5fd88a28
Werner91 <Werner91@googlemail.com>
2017-05-30 00:15:38 +0200

    Groups are now listet while ngOnInit is calling

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -9,33 +9,92 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require("@angular/core");
+require("rxjs/add/observable/of");
 require("rxjs/add/operator/catch");
 require("rxjs/add/observable/throw");
 var MyHue = new huepi();
 var AppComponent = (function () {
     function AppComponent() {
         this.name = 'Angular';
-        this.ConnectToHueBridge(MyHue);
-        this.groups = MyHue.Groups;
-        //console.log(this.groups);
-        for (var g in this.groups) {
-            console.log(g);
-        }
-        //MyHue.GroupOff(0);
+        this.lightgroups = [];
     }
+    AppComponent.prototype.ngOnInit = function () {
+        var _this = this;
+        this.ConnectToHueBridge(MyHue)
+            .then(function () { return _this.listAllGroups(); })
+            .catch(function (error) {
+            _this.ConnectToHueBridgeError = error;
+            console.log(_this.ConnectToHueBridgeError);
+        });
+    };
+    AppComponent.prototype.ConnectToHueBridge = function (MyHue) {
+        return new Promise(function (resolve, reject) {
+            if (!localStorage.MyHueBridgeIP) {
+                console.log("Trying to discover HUE Bridge via Hue Portal");
+                //Get brdige IP
+                MyHue.PortalDiscoverLocalBridges().then(function BridgesDiscovered() {
+                    console.log("Bridge IP: " + MyHue.BridgeIP);
+                    //Get bridge Configuration
+                    MyHue.BridgeGetConfig().then(function BridgeConfigReceived() {
+                        console.log("Brdige name: " + MyHue.BridgeName);
+                        //Get Bridge Data
+                        MyHue.BridgeGetData().then(function BridgeDataReceived() {
+                            localStorage.MyHueBridgeIP = MyHue.BridgeIP; // Chaching Bridge IP
+                            console.log("Received data from bridge");
+                            resolve();
+                        }, function UnableToRetreiveBridgeData() {
+                            MyHue.BridgeCreateUser().then(function BridgeUserCreated() {
+                                localStorage.MyHueBridgeIP = MyHue.BridgeIP; //Chache Bridge IP
+                                reject("Unable to receive bridge data, please press the connect button on the hue brdige ");
+                            }, function UnableToCreateUseronBridge() {
+                                reject("Unable to create new User, please press the connect button on the hue bridge ");
+                            });
+                        });
+                    }, function UnableToRetreiveBridgeConfig() {
+                        reject("Unable to receive bridge config, please press the connect button on the hue bridge ");
+                    });
+                }, function UnableToDiscoverLocalBridgesViaPortal() {
+                    reject("Error discovering bridge IP, please press the connect button on the hue bridge ");
+                });
+            }
+            else {
+                console.log("Using chached Bridge IP");
+                MyHue.BridgeIP = localStorage.MyHueBridgeIP;
+                console.log("Cached Bridge IP: " + MyHue.BridgeIP);
+                MyHue.BridgeGetConfig().then(function CachedBridgeConfigReceived() {
+                    console.log("Bridge Name: " + MyHue.BridgeName);
+                    MyHue.BridgeGetData().then(function ChachedBridgeDataReveiced() {
+                        console.log("connected");
+                        resolve();
+                    }, function UnableToRetreiveCachedBridgeData() {
+                        delete localStorage.MyHueBridgeIP; // not Whitelisted anymore
+                        reject("Unable to Retreive Cached Bridge Data");
+                    });
+                }, function UnableToRetreiveBridgeConfig() {
+                    delete localStorage.MyHueBridgeIP; //not found anymore
+                    reject("Unable to receive chached bridge config");
+                });
+            }
+        });
+    };
+    AppComponent.prototype.listAllGroups = function () {
+        console.log(MyHue.Groups);
+        for (var g in MyHue.Groups) {
+            console.log(g);
+            console.log(MyHue.Groups[g].name);
+            this.lightgroups.push(g);
+        }
+        console.log("done loading groups");
+    };
     AppComponent.prototype.onLightSwitchOn = function () {
-        MyHue.GroupOn(0);
+        MyHue.GroupOn(this.lightgroups[4]);
         MyHue.GroupSetCT(0, 467);
         MyHue.GroupSetBrightness(0, 144);
         console.log("done on");
     };
     AppComponent.prototype.onLightSwitchOff = function () {
-        MyHue.GroupOff(0);
+        MyHue.GroupOff(this.lightgroups[4]);
         console.log("done off");
-    };
-    AppComponent.prototype.DemoBehaviour = function () {
-        MyHue.GroupOff(0);
-        console.log("done");
     };
     AppComponent.prototype.StatusHeartbeat = function (MyHue) {
         MyHue.BridgeGetData().then(function UpdateUI() {
@@ -53,64 +112,12 @@ var AppComponent = (function () {
             }, 100);
         });
     };
-    AppComponent.prototype.ConnectToHueBridge = function (MyHue) {
-        if (!localStorage.MyHueBridgeIP) {
-            console.log("Trying to discover HUE Bridge via Hue Portal");
-            //Get brdige IP
-            MyHue.PortalDiscoverLocalBridges().then(function BridgesDiscovered() {
-                console.log("Bridge IP: " + MyHue.BridgeIP);
-                //Get bridge Configuration
-                MyHue.BridgeGetConfig().then(function BridgeConfigReceived() {
-                    console.log("Brdige name: " + MyHue.BridgeName);
-                    //Get Bridge Data
-                    MyHue.BridgeGetData().then(function BridgeDataReceived() {
-                        localStorage.MyHueBridgeIP = MyHue.BridgeIP; // Chaching Bridge IP
-                        console.log("Received data from bridge");
-                    }, function UnableToRetreiveBridgeData() {
-                        console.log("Unable to receive bridge data, please press the connect button on the hue brdige ");
-                        MyHue.BridgeCreateUser().then(function BridgeUserCreated() {
-                            localStorage.MyHueBridgeIP = MyHue.BridgeIP; //Chache Bridge IP
-                            console.log("Connected to the Bridge: " + MyHue.UserName);
-                            return;
-                        }, function UnableToCreateUseronBridge() {
-                            console.log("Unable to create new User, please press the connect button on the hue bridge ");
-                            return;
-                        });
-                    });
-                }, function UnableToRetreiveBridgeConfig() {
-                    console.log("Unable to receive bridge config, please press the connect button on the hue bridge ");
-                    return;
-                });
-            }, function UnableToDiscoverLocalBridgesViaPortal() {
-                console.log("Error discovering bridge IP, please press the connect button on the hue bridge ");
-                return;
-            });
-        }
-        else {
-            console.log("Using chached Bridge IP");
-            MyHue.BridgeIP = localStorage.MyHueBridgeIP;
-            console.log("Cached Bridge IP: " + MyHue.BridgeIP);
-            MyHue.BridgeGetConfig().then(function CachedBridgeConfigReceived() {
-                console.log("Bridge Name: " + MyHue.BridgeName);
-                MyHue.BridgeGetData().then(function ChachedBridgeDataReveiced() {
-                    console.log("connected");
-                }, function UnableToRetreiveCachedBridgeData() {
-                    delete localStorage.MyHueBridgeIP; // not Whitelisted anymore
-                    console.log("Unable to Retreive Cached Bridge Data");
-                    return;
-                });
-            }, function UnableToRetreiveBridgeConfig() {
-                delete localStorage.MyHueBridgeIP; //not found anymore
-                console.log("Unable to receive chached bridge config");
-            });
-        }
-    };
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "\n    <h1>Hello {{name}}</h1>\n    <div>\n      <ul>\n        <li *ngFor=\"let group of groups\">\n          {{group}}\n        </li>\n      </ul>\n    </div>\n    <Button (click)=\"onLightSwitchOn()\">On</Button>\n    <Button (click)=\"onLightSwitchOff()\">Off</Button>",
+        template: "\n    <h1>Hello {{name}}</h1>\n    <div>\n      <ul>\n        <li *ngFor=\"let group of lightgroups\">\n          {{group}}\n        </li>\n      </ul>\n    </div>\n    <Button (click)=\"onLightSwitchOn()\">On</Button>\n    <Button (click)=\"onLightSwitchOff()\">Off</Button>\n    <Button (click)=\"listAllGroups()\"> Get Groups</Button>",
     }),
     __metadata("design:paramtypes", [])
 ], AppComponent);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
 
 declare var huepi:any;
 declare var jQuery:any;
 
-var MyHue = new huepi();
+var MyHue:any = new huepi();
 
 @Component({
   selector: 'my-app',
@@ -14,46 +15,104 @@ var MyHue = new huepi();
     <h1>Hello {{name}}</h1>
     <div>
       <ul>
-        <li *ngFor="let group of groups">
+        <li *ngFor="let group of lightgroups">
           {{group}}
         </li>
       </ul>
     </div>
     <Button (click)="onLightSwitchOn()">On</Button>
-    <Button (click)="onLightSwitchOff()">Off</Button>`,
+    <Button (click)="onLightSwitchOff()">Off</Button>
+    <Button (click)="listAllGroups()"> Get Groups</Button>`,
 })
-  export class AppComponent  {
+  export class AppComponent implements OnInit {
     name = 'Angular';
     HeartbeatInterval:any;
-    groups: string[];
+    lightgroups:any[];
+    ConnectToHueBridgeError:string;
 
     constructor(){
-      this.ConnectToHueBridge(MyHue);
-      this.groups = MyHue.Groups;
-      //console.log(this.groups);
-      for(var g in this.groups){
-        console.log(g);
-      }
+      this.lightgroups = [];
+    }
 
-      //MyHue.GroupOff(0);
+    ngOnInit(){
+      this.ConnectToHueBridge(MyHue)
+        .then(() => this.listAllGroups())
+        .catch(error => {
+          this.ConnectToHueBridgeError = error;
+          console.log(this.ConnectToHueBridgeError);
+        });
+    }
+
+    ConnectToHueBridge(MyHue:any){
+      return new Promise((resolve, reject) => {
+        if(!(localStorage as any).MyHueBridgeIP){ // No chaced BridgeIP?
+          console.log("Trying to discover HUE Bridge via Hue Portal")
+          //Get brdige IP
+          MyHue.PortalDiscoverLocalBridges().then(function BridgesDiscovered(){
+            console.log("Bridge IP: " + MyHue.BridgeIP);
+            //Get bridge Configuration
+            MyHue.BridgeGetConfig().then(function BridgeConfigReceived(){
+              console.log("Brdige name: " + MyHue.BridgeName);
+              //Get Bridge Data
+              MyHue.BridgeGetData().then(function BridgeDataReceived(){
+                (localStorage as any).MyHueBridgeIP = MyHue.BridgeIP; // Chaching Bridge IP
+                console.log("Received data from bridge");
+                resolve();
+              }, function UnableToRetreiveBridgeData(){
+                MyHue.BridgeCreateUser().then(function BridgeUserCreated(){
+                  (localStorage as any).MyHueBridgeIP = MyHue.BridgeIP; //Chache Bridge IP
+                  reject("Unable to receive bridge data, please press the connect button on the hue brdige ")
+                }, function UnableToCreateUseronBridge() {
+                  reject("Unable to create new User, please press the connect button on the hue bridge ");
+                });
+              });
+            }, function UnableToRetreiveBridgeConfig() {
+                reject("Unable to receive bridge config, please press the connect button on the hue bridge ");
+            });
+          }, function UnableToDiscoverLocalBridgesViaPortal(){
+              reject("Error discovering bridge IP, please press the connect button on the hue bridge ");
+          });
+        }else{// Using chached Bridge IP
+          console.log("Using chached Bridge IP");
+          MyHue.BridgeIP = (localStorage as any).MyHueBridgeIP;
+          console.log("Cached Bridge IP: " + MyHue.BridgeIP);
+          MyHue.BridgeGetConfig().then(function CachedBridgeConfigReceived(){
+            console.log("Bridge Name: " + MyHue.BridgeName);
+            MyHue.BridgeGetData().then(function ChachedBridgeDataReveiced(){
+              console.log("connected");
+              resolve();
+            }, function UnableToRetreiveCachedBridgeData(){
+                delete (localStorage as any).MyHueBridgeIP // not Whitelisted anymore
+                reject("Unable to Retreive Cached Bridge Data");
+            });
+          }, function UnableToRetreiveBridgeConfig(){
+              delete (localStorage as any).MyHueBridgeIP; //not found anymore
+              reject("Unable to receive chached bridge config");
+          });
+        }
+      });
+    }
+
+    listAllGroups(){
+      console.log(MyHue.Groups);
+      for(var g in MyHue.Groups){
+        console.log(g);
+        console.log(MyHue.Groups[g].name);
+        this.lightgroups.push(g);
+      }
+      console.log("done loading groups");
     }
 
     onLightSwitchOn() {
-      MyHue.GroupOn(0);
+      MyHue.GroupOn(this.lightgroups[4]);
       MyHue.GroupSetCT(0, 467);
       MyHue.GroupSetBrightness(0, 144);
       console.log("done on");
     }
 
     onLightSwitchOff() {
-      MyHue.GroupOff(0);
+      MyHue.GroupOff(this.lightgroups[4]);
       console.log("done off");
-    }
-
-    DemoBehaviour() {
-      MyHue.GroupOff(0);
-
-      console.log("done");
     }
 
 
@@ -74,95 +133,4 @@ var MyHue = new huepi();
         }, 100);
       });
     }
-
-ConnectToHueBridge(MyHue:any){
-  if(!(localStorage as any).MyHueBridgeIP){ // No chaced BridgeIP?
-    console.log("Trying to discover HUE Bridge via Hue Portal")
-    //Get brdige IP
-    MyHue.PortalDiscoverLocalBridges().then(function BridgesDiscovered(){
-      console.log("Bridge IP: " + MyHue.BridgeIP);
-      //Get bridge Configuration
-      MyHue.BridgeGetConfig().then(function BridgeConfigReceived(){
-        console.log("Brdige name: " + MyHue.BridgeName);
-        //Get Bridge Data
-        MyHue.BridgeGetData().then(function BridgeDataReceived(){
-          (localStorage as any).MyHueBridgeIP = MyHue.BridgeIP; // Chaching Bridge IP
-          console.log("Received data from bridge");
-        }, function UnableToRetreiveBridgeData(){
-          console.log ("Unable to receive bridge data, please press the connect button on the hue brdige ");
-          MyHue.BridgeCreateUser().then(function BridgeUserCreated(){
-            (localStorage as any).MyHueBridgeIP = MyHue.BridgeIP; //Chache Bridge IP
-            console.log("Connected to the Bridge: " + MyHue.UserName);
-            return;
-          }, function UnableToCreateUseronBridge() {
-            console.log("Unable to create new User, please press the connect button on the hue bridge ");
-            return;
-          });
-        });
-      }, function UnableToRetreiveBridgeConfig() {
-          console.log ("Unable to receive bridge config, please press the connect button on the hue bridge ");
-          return;
-      });
-    }, function UnableToDiscoverLocalBridgesViaPortal(){
-        console.log("Error discovering bridge IP, please press the connect button on the hue bridge ");
-        return;
-    });
-  }else{// Using chached Bridge IP
-    console.log("Using chached Bridge IP");
-    MyHue.BridgeIP = (localStorage as any).MyHueBridgeIP;
-    console.log("Cached Bridge IP: " + MyHue.BridgeIP);
-    MyHue.BridgeGetConfig().then(function CachedBridgeConfigReceived(){
-      console.log("Bridge Name: " + MyHue.BridgeName);
-      MyHue.BridgeGetData().then(function ChachedBridgeDataReveiced(){
-        console.log("connected");
-      }, function UnableToRetreiveCachedBridgeData(){
-          delete (localStorage as any).MyHueBridgeIP // not Whitelisted anymore
-          console.log("Unable to Retreive Cached Bridge Data");
-          return;
-      });
-    }, function UnableToRetreiveBridgeConfig(){
-        delete (localStorage as any).MyHueBridgeIP; //not found anymore
-        console.log("Unable to receive chached bridge config");
-    });
-  }
-}
-
-
-
-/*
-  //Checks if there is a Hue bridge in the network
-  discoverLocalBridge(hue:any){
-    let bridgeIP:string;
-    hue.discover().then((bridges:any) => {
-      if(bridges.length === 0) {
-          console.log('No bridges found. :(');
-      }
-      else {
-          (bridges.forEach((b:any) => {
-            console.log('Bridge found at IP address ' + b.internalipaddress);
-            bridgeIP = b.internalipaddress;
-          }));
-          //ToDo: What happens if there als multiple Briges
-
-          this.createNewUser(hue, bridgeIP);
-      }
-    }).catch((e:any) => console.log('Error finding bridges', e));
-  }
-
-  createNewUser(hue:any, bridgeIP:string){
-
-    var bridge = hue.bridge(bridgeIP);
-    // create user account (requires link button to be pressed)
-    bridge.createUser('myApp#testdevice').then((data:any) => {
-      // extract bridge-generated username from returned data
-      var username = data[0].success.username;
-
-      console.log('New username:', username);
-
-      // instantiate user object with username
-      var user = bridge.user(username);
-    }).catch((e:any) => console.log("There was an error by creating a new user", e));
-  }
-  */
-
 }
